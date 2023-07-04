8f6b82aa14d86e85c92eefe52096b8a651d03a70
IsisTheOne <alenacard619@gmail.com>
2023-07-04 14:04:53 -0300

    04/07/2023

---
+++ android/app/src/main/kotlin/com/example/cronograma_form/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.cronograma_form
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Cadform());
+}
+
+class Cadform extends StatelessWidget {
+  const Cadform({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
