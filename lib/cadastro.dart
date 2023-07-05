c15895dccfa94d816bd3531146b30040d61bb60d
IsisTheOne <alenacard619@gmail.com>
2023-07-05 17:15:58 -0300

    3 archives

---
+++ lib/cadastro.dart
@@ -0,0 +1,96 @@
+import 'package:cronograma_form/pagina.dart';
+import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  final _formkey = GlobalKey<FormState>();
+  final TextEditingController id_curso = TextEditingController();
+  final TextEditingController descricao_curso = TextEditingController();
+  final TextEditingController carga_horaria_curso = TextEditingController();
+  final TextEditingController modalidade = TextEditingController();
+  final TextEditingController eixo = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("BaseApp"),
+        backgroundColor: const Color.fromRGBO(22, 85, 140, 14),
+      ),
+      body: Container(
+        padding: const EdgeInsets.all(16),
+        child: Center(
+          child: Form(
+              key: _formkey,
+              child: Column(children: [
+                TextFormField(
+                  controller: id_curso,
+                  decoration: const InputDecoration(labelText: "Curso", border: OutlineInputBorder(),prefixIcon: Icon(Icons.abc)),
+                  validator: (value) {
+                      if (value!.isEmpty) {
+                        return "campo obrigatório";
+                      }
+                    },
+                ),
+                TextFormField(
+                  controller: descricao_curso,
+                  decoration: const InputDecoration(labelText: "Descrição", border: OutlineInputBorder(),prefixIcon: Icon(Icons.abc)),
+                  validator: (value) {
+                      if (value!.isEmpty) {
+                        return "campo obrigatório";
+                      }
+                    },
+                ),
+                TextFormField(
+                    controller: carga_horaria_curso,
+                    decoration:
+                        const InputDecoration(labelText: "Carga Horária", border: OutlineInputBorder(),prefixIcon: Icon(Icons.abc)),
+                    keyboardType: TextInputType.number,
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return "campo obrigatório";
+                      }
+                    },
+                        ),
+                TextFormField(
+                  controller: modalidade,
+                  decoration: const InputDecoration(labelText: "Modalidade", border: OutlineInputBorder(),prefixIcon: Icon(Icons.abc)),
+                  validator: (value) {
+                      if (value!.isEmpty) {
+                        return "campo obrigatório";
+                      }
+                    },
+                ),
+                TextFormField(
+                  controller: eixo,
+                  decoration:
+                      const InputDecoration(labelText: "Area De Conhecimento", border: OutlineInputBorder(),prefixIcon: Icon(Icons.abc)),
+                      validator: (value) {
+                      if (value!.isEmpty) {
+                        return "campo obrigatório";
+                      }
+                    },
+                ),
+                ElevatedButton(
+                    onPressed: () {
+                      if (_formkey.currentState!.validate()) {
+                        Fluttertoast.showToast(
+                          msg: "Curso Cadastrado com sucesso",
+                          gravity: ToastGravity.TOP_RIGHT,
+                          backgroundColor: Colors.amber,
+                        );
+                      }
+                    },
+                    child: const Text("Cadastrar"))
+              ])),
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:cronograma_form/pagina.dart';
 import 'package:flutter/material.dart';
-import 'package:fluttertoast/fluttertoast.dart';
 void main() {
   runApp(const Cadform());
 }
@@ -21,95 +20,4 @@ class Cadform extends StatelessWidget {
   }
 }
 
-class Home extends StatefulWidget {
-  const Home({super.key});
 
-  @override
-  State<Home> createState() => _HomeState();
-}
-
-class _HomeState extends State<Home> {
-  final _formkey = GlobalKey<FormState>();
-  final TextEditingController id_curso = TextEditingController();
-  final TextEditingController descricao_curso = TextEditingController();
-  final TextEditingController carga_horaria_curso = TextEditingController();
-  final TextEditingController modalidade = TextEditingController();
-  final TextEditingController eixo = TextEditingController();
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("BaseApp"),
-        backgroundColor: const Color.fromRGBO(22, 85, 140, 14),
-      ),
-      body: Container(
-        padding: const EdgeInsets.all(16),
-        child: Center(
-          child: Form(
-              key: _formkey,
-              child: Column(children: [
-                TextFormField(
-                  controller: id_curso,
-                  decoration: const InputDecoration(labelText: "Curso", border: OutlineInputBorder(),prefixIcon: Icon(Icons.abc)),
-                  validator: (value) {
-                      if (value!.isEmpty) {
-                        return "campo obrigatório";
-                      }
-                    },
-                ),
-                TextFormField(
-                  controller: descricao_curso,
-                  decoration: const InputDecoration(labelText: "Descrição", border: OutlineInputBorder(),prefixIcon: Icon(Icons.abc)),
-                  validator: (value) {
-                      if (value!.isEmpty) {
-                        return "campo obrigatório";
-                      }
-                    },
-                ),
-                TextFormField(
-                    controller: carga_horaria_curso,
-                    decoration:
-                        const InputDecoration(labelText: "Carga Horária", border: OutlineInputBorder(),prefixIcon: Icon(Icons.abc)),
-                    keyboardType: TextInputType.number,
-                    validator: (value) {
-                      if (value!.isEmpty) {
-                        return "campo obrigatório";
-                      }
-                    },
-                        ),
-                TextFormField(
-                  controller: modalidade,
-                  decoration: const InputDecoration(labelText: "Modalidade", border: OutlineInputBorder(),prefixIcon: Icon(Icons.abc)),
-                  validator: (value) {
-                      if (value!.isEmpty) {
-                        return "campo obrigatório";
-                      }
-                    },
-                ),
-                TextFormField(
-                  controller: eixo,
-                  decoration:
-                      const InputDecoration(labelText: "Area De Conhecimento", border: OutlineInputBorder(),prefixIcon: Icon(Icons.abc)),
-                      validator: (value) {
-                      if (value!.isEmpty) {
-                        return "campo obrigatório";
-                      }
-                    },
-                ),
-                ElevatedButton(
-                    onPressed: () {
-                      if (_formkey.currentState!.validate()) {
-                        Fluttertoast.showToast(
-                          msg: "Curso Cadastrado com sucesso",
-                          gravity: ToastGravity.TOP_RIGHT,
-                          backgroundColor: Colors.amber,
-                        );
-                      }
-                    },
-                    child: const Text("Cadastrar"))
-              ])),
-        ),
-      ),
-    );
-  }
-}
